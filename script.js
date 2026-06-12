6bffbb7fec620cb9b48a3d377867d4c6cd65a86d
heloisaviebrantz <heloviebrantz@gmail.com>
2026-06-12 17:12:39 -0300

    .x

--- script.js
+++ script.js
@@ -104,3 +104,22 @@ function revelar(){
 }
 
 revelar(); 
+
+
+const linksMenu = document.querySelectorAll('nav a[href^="#"]');
+
+linksMenu.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+}); 
