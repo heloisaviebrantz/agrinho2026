685888716edd17b7ae4564b53f4bba99ad23500c
heloisaviebrantz <heloviebrantz@gmail.com>
2026-06-12 10:17:17 -0300

    .x

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ function aumentarFonte(){
     tamanho += 10;
     document.body.style.fontSize = tamanho + "%";
 }
+
+const ctx = document.getElementById('graficoCarbono');
+
+new Chart(ctx, {
+    type: 'line',
+
+    data: {
+        labels: ['2022', '2023', '2024', '2025', '2026'],
+
+        datasets: [{
+            label: 'Mercado de Carbono (bilhões de dólares)',
+
+            data: [1.1, 1.6, 2.3, 3.2, 4.1],
+
+            borderColor: '#1B5E20',
+
+            backgroundColor: 'rgba(46,125,50,0.2)',
+
+            borderWidth: 4,
+
+            fill: true,
+            tension: 0.4
+        }]
+    },
+
+    options: {
+
+        responsive: true,
+
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 14
+                    }
+                }
+            }
+        }
+    }
+});
+
