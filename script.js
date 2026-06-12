d31ccc03036318fff2aac6f8dd0c173d375483cf
heloisaviebrantz <heloviebrantz@gmail.com>
2026-06-12 08:12:52 -0300

    .x

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+
+let tamanho = 100;
+
+function modoNoturno(){
+    document.body.classList.toggle("dark-mode");
+}
+
+function aumentarFonte(){
+    tamanho += 10;
+    document.body.style.fontSize = tamanho + "%";
+}
+
+
+
+</script>
